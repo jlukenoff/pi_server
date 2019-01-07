c65ea43cc116067fd6c984adf2d2ea0d47635fac
jlukenoff <jlukenoff@gmail.com>
2019-01-07 13:28:49 -0800

    Sync w/pi source

--- server/index.js
+++ server/index.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const jsonParser = require('body-parser').json();
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../public')));
+// Config
+app.use(express.static(path.resolve(__dirname, '../html')));
 
 app.get('/', (req, res) => {
   res.end('request received');
 });
 
+app.post('/api/target', jsonParser, (req, res) => {
+  const {
+    summary,
+    description,
+    created_by: createdBy,
+    created_at: createdAt,
+  } = req.body;
+  console.log(summary);
+  console.log(description);
+  console.log(createdBy);
+  console.log(createdAt);
+_____________________________
+  fs.appendFile(
+    './data/events.csv',
+    `${[summary, description, createdBy, createdAt].join('|')}\n`,
+    err => {
+      console.error(`Error writing to file: ${err}`);
+    }
+  );
+});
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => console.log(`Server running on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+);
 
 if (process.env.NODE_ENV === 'test') server.close();
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const ENTRY_POINT = path.resolve(__dirname, 'client/index.jsx');
 
-const OUTPUT_DIR = path.resolve(__dirname, 'public');
+const OUTPUT_DIR = path.resolve(__dirname, 'html');
 
 module.exports = {
   entry: ENTRY_POINT,
@@ -14,7 +14,7 @@ module.exports = {
   mode: 'development',
   resolve: { extensions: ['.js', '.jsx'] },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, 'html'),
     compress: true,
     port: 8080,
   },
