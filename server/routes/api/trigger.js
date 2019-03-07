a928b5ef8872475e5e7f8e4e79230f433ed1335d
jlukenoff <jlukenoff@gmail.com>
2019-03-06 20:45:17 -0800

    Parse argument from calendar trigger and make calls to IoT helpers

--- server/routes/api/trigger.js
+++ server/routes/api/trigger.js
@@ -1,59 +1,96 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path = require('path');
 // modules for interacting w/the HUE bridge api
-const hueControls = require('./hue_controls');
+const { getAllLights, adjustLight, toggleLight } = require('./hue_controls');
 const { authToken } = require('./credentials.json');
+
 const atob = require('atob');
 
+// POST - used to toggle or adjust lights from a calendar event
 router.post('/', (req, res) => {
-  // extract values from body
   const {
-    summary,
-    description,
+    summary: service,
+    description: csvArgs,
     begins_at: beginsAt,
     created_by: createdBy,
     created_at: createdAt,
   } = req.body;
 
-  // parse auth
   const token = req.header('Authorization');
   const unamePass = atob(token.split(' ')[1]);
   console.log(unamePass);
-
-  // verify auth
   if (unamePass !== authToken) {
-    res.sendStatus(401);
+    return res.sendStatus(401);
   }
 
-  if (process.env.NODE_ENV === 'production') {
-    // write to logs
-    fs.appendFile(
-      path.join('/home/pi', 'Desktop', 'events.csv'),
-      `${[
-        summary,
-        description,
-        createdBy,
-        createdAt,
-        beginsAt,
-        new Date().toISOString(),
-      ].join('|')}\n`,
-      err => {
-        if (err) console.error(`Error writing to log: ${err}`);
-        // TODO:
-        // parse arguments received in API call
-        // make calls to IoT
-        console.log(
-          `successfully processed payload: ${JSON.stringify(req.body, null, 2)}`
-        );
-        res.send('SUCCESS: wrote to logs');
-      }
-    );
-  } else {
-    res.send(
-      `SUCCESS: received payload - ${JSON.stringify(req.body, null, 2)}`
-    );
+  if (service === 'LIGHT') {
+    csvArgs
+      .split('\n')
+      .slice(1)
+      .forEach(row => {
+        console.log('called service functions');
+        const [lightID, bri, on] = row.split('|');
+        if (bri) {
+          return adjustLight(lightID, bri, function(err, hueResponse) {
+            if (err) {
+              return console.error(
+                `Error adjusting light ${lightID} to brightness ${bri}: ${err}`
+              );
+            }
+            console.log(
+              `Successfully adjusted light id ${lightID} to brightness ${bri} (0-254 range).\nHUE response: ${JSON.stringify(
+                hueResponse,
+                null,
+                2
+              )}`
+            );
+          });
+        } else {
+          return toggleLight(lightID, on, function(err, hueResponse) {
+            if (err) {
+              console.error(
+                `Error toggling light ${lightID} to state on = ${on}: ${err}`
+              );
+            }
+            console.log(
+              `Successfully toggling light id ${lightID} to state = ${on} (0-254 range).\nHUE response: ${JSON.stringify(
+                hueResponse,
+                null,
+                2
+              )}`
+            );
+
+            console.log('----------');
+
+            return res.send('SUCCESS');
+          });
+        }
+      });
   }
+
+  // write to logs
+  /*  fs.appendFile(
+    '../../data/events.csv',
+    `${[
+      service,
+      csvArgs,
+      createdBy,
+      createdAt,
+      beginsAt,
+      new Date().toISOString(),
+    ].join(',')}\n`,
+    function(err) {
+      if (err) return console.error(`Error writing to log: ${err}`);
+      console.log(
+        `successfully processed payload: ${JSON.stringify(req.body, null, 2)}`
+      );
+      console.log('SUCCESS: wrote to logs');
+      res.send(
+        `SUCCESS: received payload - ${JSON.stringify(req.body, null, 2)}`
+      );
+    }
+  ); */
 });
-
 module.exports = router;
