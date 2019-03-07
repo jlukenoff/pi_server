dc19bcaab3584ada18c89eaac2568e6b7b964a5c
jlukenoff <jlukenoff@gmail.com>
2019-03-06 20:45:48 -0800

    Convert arg types to be correct if passed in as strings

--- server/routes/api/hue_controls/index.js
+++ server/routes/api/hue_controls/index.js
@@ -1,31 +1,37 @@
 const fetch = require('node-fetch');
 const { HUE_USERNAME } = require('../credentials.json');
 
-const getAllLights = next => {
+const getAllLights = done => {
   fetch(`http://10.0.0.218/api/${HUE_USERNAME}/lights`)
     .then(chunk => chunk.json())
-    .then(data => next(null, data))
-    .catch(err => next(err));
+    .then(data => done(null, data))
+    .catch(err => done(err));
 };
 
-const adjustLight = (lightID, bri, next) => {
+const adjustLight = (lightID, bri, done) => {
+  if (typeof bri !== 'number') {
+    bri = Number(bri);
+  }
   fetch(`http://10.0.0.218/api/${HUE_USERNAME}/lights/${lightID}/state`, {
     method: 'PUT',
     body: JSON.stringify({ bri }),
   })
     .then(chunk => chunk.json())
-    .then(data => next(null, data))
-    .catch(err => next(err));
+    .then(data => done(null, data))
+    .catch(err => done(err));
 };
 
-const toggleLight = (lightID, on, next) => {
+const toggleLight = (lightID, on, done) => {
+  if (typeof on !== 'boolean') {
+    on = 'true' === on;
+  }
   fetch(`http://10.0.0.218/api/${HUE_USERNAME}/lights/${lightID}/state`, {
     method: 'PUT',
     body: JSON.stringify({ on }),
   })
     .then(chunk => chunk.json())
-    .then(data => next(null, data))
-    .catch(err => next(err));
+    .then(data => done(null, data))
+    .catch(err => done(err));
 };
 
 module.exports = { getAllLights, adjustLight, toggleLight };
