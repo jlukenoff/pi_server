32faa168fed276afe0f24d7906566e23ba6b8611
jlukenoff <jlukenoff@gmail.com>
2019-03-24 15:42:50 -0700

    Add console.logs

--- server/routes/api/water_pump.js
+++ server/routes/api/water_pump.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const { timeOn } = req.body;
+  console.log(timeOn);
   return fetch('http://10.0.0.180:3000/api/pump', {
     method: 'POST',
     body: JSON.stringify({ timeOn }),
