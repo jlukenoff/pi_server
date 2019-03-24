ea666afa356500dbcf84ded35e5e45ecad901e4d
jlukenoff <jlukenoff@gmail.com>
2019-03-24 15:19:58 -0700

    Refactor to make call to GPIO API

--- server/routes/api/water_pump.js
+++ server/routes/api/water_pump.js
@@ -5,14 +5,13 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const { timeOn } = req.body;
-  const relay = new Gpio(4, 'high');
-  console.log(`current relay voltage: ${relay.readSync()}`);
-  relay.writeSync(0);
-  setTimeout(() => {
-    relay.writeSync(1);
-    relay.unexport();
-    res.json({ status: 'SUCCESS', message: `Pump was on for ${timeOn}` });
-  }, timeOn);
+  return fetch('http://10.0.0.180:3000/api/pump', {
+    method: 'POST',
+    body: JSON.stringify({ timeOn }),
+  })
+    .then(c => c.json())
+    .then(d => res.json(d))
+    .catch(e => console.error(`Error communicating with GPIO API: ${e}`));
 });
 
 module.exports = router;
