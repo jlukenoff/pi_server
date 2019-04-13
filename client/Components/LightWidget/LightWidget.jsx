61121c1d4939b6a7d71ad8268d194915ec3faa65
jlukenoff <jlukenoff@gmail.com>
2019-04-13 14:41:18 -0700

    FE updates

--- client/Components/LightWidget/LightWidget.jsx
+++ client/Components/LightWidget/LightWidget.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { LightWidgetContainer } from '../Styles/Styles'
+import { LightWidgetContainer } from '../Styles/Styles';
 
-const LightWidget = ({
-  light,
-  name
-}) => <LightWidgetContainer>
-  Name: {name}<br/>On: {light.state.on ? 'true' : 'false'}<br />reachable: {light.state.reachable ? 'true' : 'false'}</LightWidgetContainer>;
+const InputContainer = styled.div``;
+const LightInfoContainer = styled.div``;
 
-// LightWidget.propTypes = {
-// };
+const LightWidget = ({ light, name }) => (
+  <LightWidgetContainer>
+    <LightInfoContainer>
+      Name: {name}
+      <br />
+      On: {light.state.on ? 'true' : 'false'}
+      <br />
+      reachable: {light.state.reachable ? 'true' : 'false'}
+    </LightInfoContainer>
+    <InputContainer>
+      <input
+        type="range"
+        min="0"
+        max="254"
+        value={light.state.bri}
+        onChange={e => console.log(e.target.value)}
+      />
+    </InputContainer>
+  </LightWidgetContainer>
+);
+
+LightWidget.propTypes = {
+  light: PropTypes.object,
+  name: PropTypes.string,
+};
+
+LightWidget.defaultProps = {
+  light: {},
+  name: '',
+};
 
 export default LightWidget;
